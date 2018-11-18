6d3023d55cbe6d947d897a107da079876bdb3dc3
蒋亚光 <jiangyaguang@192.168.0.12>
2018-11-18 18:12:41 +0800

    update

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const http = require('http');
 
-// const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = '172.19.56.164';
+const port = 3001;
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World? hahhh\n');
+    res.end('Hello 小大姐\n');
 });
 
-server.listen(port, () => {
+server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
